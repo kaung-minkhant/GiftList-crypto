bc1b1cf3290bfa42e6fe1113c568ee59a806c08d
Kaung Min Khant <133316096+kaung-minkhant@users.noreply.github.com>
2023-12-16 09:26:43 +0000

    added proof and leaf in client call, and verify in server

--- client/index.js
+++ client/index.js
@@ -2,14 +2,20 @@ const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
-const serverUrl = 'http://localhost:1225';
+// const serverUrl = 'http://localhost:1225';
+const serverUrl = 'https://silver-guacamole-ggprpgq4pgrcw9g5-1225.app.github.dev'
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  const personName = "Sidney Kertzmann";
+  const index = niceList.findIndex(n => n === personName);
+  const proof = merkleTree.getProof(index)
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+  const {data: gift} = await axios.post(`${serverUrl}/gift`, {
+    proof,
+    leaf: personName,
   });
+  
 
   console.log({ gift });
 }
--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
-
+const cors = require('cors')
 const port = 1225;
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
-
+const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
+app.get('/', (req, res) => {
+  res.status(200).send('you ping, i pong');
+})
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
-
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const {proof, leaf} = body
+  const isInTheList = verifyProof(proof, leaf, MERKLE_ROOT);;
+  
   if(isInTheList) {
-    res.send("You got a toy robot!");
+    return res.status(200).send("You got a toy robot!");
   }
   else {
-    res.send("You are not on the list :(");
+    return res.status(200).send("You are not on the list :(");
   }
 });
 
